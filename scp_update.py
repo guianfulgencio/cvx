198a032e8d46b20062c624451ef6e84d867276e1
Guian <guian_fulgencio@yahoo.com>
2023-04-12 04:01:31 +0800

    updates

--- scp_update.py
+++ scp_update.py
@@ -1,4 +1,5 @@
 import csv
+import argparse
 from rich import print as rprint
 from netmiko import ConnectHandler, NetmikoTimeoutException, NetmikoAuthenticationException
 from concurrent.futures import ThreadPoolExecutor
@@ -21,6 +22,17 @@ def configure_scp_server(hostname, device_type, username, password):
         rprint(f'[red]❌ Failed to configure SCP server on {hostname}: {e}')
 
 if __name__ == '__main__':
+    ##########################
+    # Script Arguments
+    ##########################
+    parser = argparse.ArgumentParser(description="Credential to update ACL")
+    parser.add_argument('-u', '--username', type=str, metavar='',\
+        help='Username to access network device', required=True)
+    parser.add_argument('-p', '--password', type=str, metavar='',\
+        help='Password to access network device', required=True)
+    args = parser.parse_args()
+    username = args.username
+    password = args.password
     with open('devices.csv', 'r') as file:
         reader = csv.DictReader(file)
         with ThreadPoolExecutor(max_workers=100) as executor:
@@ -28,7 +40,7 @@ if __name__ == '__main__':
             for row in reader:
                 hostname = row['IP Address']
                 device_type = row['machine type'].lower()
-                future = executor.submit(configure_scp_server, hostname, device_type, username='cisco', password='cisco')
+                future = executor.submit(configure_scp_server, hostname, device_type, username=username, password=password)
                 futures.append(future)
             for future in futures:
                 future.result()
